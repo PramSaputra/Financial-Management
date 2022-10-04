2a6c0e2c7867cafc0aa1720e7ada86c0f2ba7803
PramSaputra <udipram@yahoo.com>
2022-10-04 11:19:22 +0700

    Initial Commit

---
+++ Financial Management/Financial_ManagementApp.swift
@@ -0,0 +1,20 @@
+//
+//  Financial_ManagementApp.swift
+//  Financial Management
+//
+//  Created by Pramudya Prameswara Trisna Saputra on 04/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Financial_ManagementApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
